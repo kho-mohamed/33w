f55ce265d515b8bc88ba87765dfcbb8fc2077351
kho-mohamed <simopolo4@gmail.com>
2025-05-18 21:08:50 -0400

    étape 2 du devis

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="piedpage">
+    <p class="piedpage__paragraphe">Tous droits réservés &copy; 2025</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,85 @@
+<?php get_header(); ?>
+<main>
+  <section class="hero">
+    <div class="hero__content">
+      <h1 class="hero__titre">Voyagez Autrement avec Club de Voyage !</h1>
+      <p class="hero__description">
+        Découvrez des destinations inoubliables avec Club de Voyage. Nous
+        vous offrons des expériences uniques, des séjours authentiques et
+        des paysages à couper le souffle. Partez à la découverte du monde
+        avec nous et créez des souvenirs impérissables.
+      </p>
+      <div class="hero__contact">
+        <a href="mailto:info@clubdevoyage.ca">info@clubdevoyage.ca</a>
+        <p>123, rue des Explorateurs, Montréal</p>
+        <p>514-123-4567</p>
+      </div>
+      <a href="#" class="button">S'inscrire</a>
+      <div class="hero__reseaux-sociaux">
+        <a href="#" class=""><img src="https://s2.svgbox.net/materialui.svg?ic=facebook&color=000" width="32"
+            height="32" /></a>
+        <a href="#" class=""><img src="https://s2.svgbox.net/social.svg?ic=instagram&color=000" width="32"
+            height="32" /></a>
+      </div>
+    </div>
+    <form action="" class="form">
+      <div class="form__champ">
+        <label for="nom">Nom</label>
+        <input type="text" id="nom" name="nom" required />
+      </div>
+      <div class="form__champ">
+        <label for="prenom">Prénom</label>
+        <input type="text" id="prenom" name="prenom" required />
+      </div>
+      <div class="form__champ">
+        <label for="email">Courriel</label>
+        <input type="email" id="email" name="email" required />
+      </div>
+      <div class="form__champ">
+        <label for="telephone">Téléphone</label>
+        <input type="tel" id="telephone" name="telephone" required />
+      </div>
+      <div class="form__submit">
+        <input type="submit" value="S'inscrire" />
+      </div>
+    </form>
+  </section>
+  <section class="galerie">
+    <h2 class="galerie__title">Découvrez nos meilleures destinations</h2>
+    <div class="galerie__content">
+      <picture class="galerie__picture"><img src="images/galerie/galerie (1).jpg" alt="1" class="galerie__img" />
+      </picture>
+      <picture class="galerie__picture"><img src="images/galerie/galerie (2).jpg" alt="2" class="galerie__img" />
+      </picture>
+      <picture class="galerie__picture"><img src="images/galerie/galerie (3).jpg" alt="3" class="galerie__img" />
+      </picture>
+      <picture class="galerie__picture"><img src="images/galerie/galerie (4).jpg" alt="4" class="galerie__img" />
+      </picture>
+      <picture class="galerie__picture"><img src="images/galerie/galerie (5).jpg" alt="5" class="galerie__img" />
+      </picture>
+      <picture class="galerie__picture"><img src="images/galerie/galerie (6).jpg" alt="6" class="galerie__img" />
+      </picture>
+      <picture class="galerie__picture"><img src="images/galerie/galerie (7).jpg" alt="7" class="galerie__img" />
+      </picture>
+      <picture class="galerie__picture"><img src="images/galerie/galerie (8).jpg" alt="8" class="galerie__img" />
+      </picture>
+      <picture class="galerie__picture"><img src="images/galerie/galerie (9).jpg" alt="9" class="galerie__img" />
+      </picture>
+      <picture class="galerie__picture"><img src="images/galerie/galerie (10).jpg" alt="10" class="galerie__img" />
+      </picture>
+    </div>
+  </section>
+  <section class="populaire">
+    <?php
+    if (have_posts()) {
+      while (have_posts()) {
+        the_post();
+        ?>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content();
+      }
+    } ?>
+  </section>
+</main>
+
+<?php get_footer();
