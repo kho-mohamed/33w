25d4a1ba4ba7966bb96d47924bb25d156487da6d
kho-mohamed <simopolo4@gmail.com>
2025-06-13 08:56:05 -0400

    ajout d'une page search.php

---
+++ search.php
@@ -0,0 +1,23 @@
+<?php
+// Modèle index.php permet d'afficher le modèle par defaut.
+get_header(); ?>
+<main>
+
+  <section class="populaire">
+    <?php
+    if (have_posts()) {
+      while (have_posts()) {
+        the_post();
+        // affiche l'image mise en avant de "post" miniature
+        the_post_thumbnail('thumbnail');
+        ?>
+        <h1><?php the_title();
+        // affiche le titre principal de "post" ?></h1>
+
+        <?php the_content();
+      }
+    } ?>
+  </section>
+</main>
+
+<?php get_footer();
