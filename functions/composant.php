c7ce662d6ebe8f67857795b381ac5b5b2a687b80
kho-mohamed <simopolo4@gmail.com>
2025-08-08 10:12:13 -0400

    correction parent_category_id

--- functions/composant.php
+++ functions/composant.php
@@ -41,9 +41,10 @@ function vague($couleur_haut, $couleur_bas)
 
 function extraire_list_categories($nom_categorie)
 {
-    $parent_category_id = get_term_by('slug', $nom_categorie, 'category');
+    // $parent_category_id = get_term_by('slug', $nom_categorie, 'category');
+    $parent_category_id = get_category_by_slug($nom_categorie);
     $tableau = array(
-        'parent' => 14,
+        'parent' => $parent_category_id->term_id,
         'hide_empty' => true,
     );
     $list_categories = get_categories($tableau);
