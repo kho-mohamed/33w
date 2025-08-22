445aed4076a7d3cc7aaf72b5ecf1395f93081637
kho-mohamed <simopolo4@gmail.com>
2025-08-22 09:35:13 -0400

    configuration du menu de la page erreur 404

--- gabarit/erreur-404.php
+++ gabarit/erreur-404.php
@@ -4,21 +4,22 @@ $erreur_404_titre = get_theme_mod('404_titre', 'Erreur 404 - Page non trouvée')
 $erreur_404_message = get_theme_mod('404_message', 'Désolé, la page que vous recherchez est introuvable.');
 $erreur_404_bg_couleur = get_theme_mod('404_bg_couleur', '#ffffff');
 ?>
-<div class="erreur-404_background"
+<div class="erreur-404__background"
     style="background-image: url('<?= $erreur_404_background ? $erreur_404_background : '' ?>'); opacity:1; background-color: <?= $erreur_404_bg_couleur ?>;">
-    <div class="erreur-404_titre">
-        <h1 class="erreur-404_titre"><?= $erreur_404_titre ?></h1>
+    <div class="erreur-404__titre">
+        <h1 class="erreur-404__titre"><?= $erreur_404_titre ?></h1>
     </div>
-    <div class="erreur-404_message">
-        <p class="erreur-404_message"><?= $erreur_404_message ?></p>
+    <div class="erreur-404__message">
+        <p class="erreur-404__message"><?= $erreur_404_message ?></p>
     </div>
-    <div class="button erreur-404_button"><a
-        href="<?= home_url(); ?>"><?= __('Retour à l\'accueil', 'club-voyagoo') ?></a>
+    <div class="button erreur-404__button"><a
+            href="<?= home_url(); ?>"><?= __('Retour à l\'accueil', 'club-voyagoo') ?></a>
     </div>
-  <section class="destination">
+    <div class="erreur-404__lien">
+        <?php wp_nav_menu(array(
+            "menu" => "selection",
+            "container" => "nav"
+        )) ?>
+    </div>
 
-    <?php extraire_list_categories('destination'); ?>
-    <h2 class="destination__titre">Articles de la catégorie</h2>
-    <div class="destination__list"></div>
-  </section>
 </div>
